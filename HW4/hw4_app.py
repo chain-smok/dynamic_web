94e54c859736c93fa02a674818c41226b9bbb627
chain-smok <s1101780@gm.pu.edu.tw>
2023-05-07 00:24:02 +0800

    註冊登入功能

--- HW4/hw4_app.py
+++ HW4/hw4_app.py
@@ -1,16 +1,25 @@
 from flask import Flask, request,render_template
 import json
-
+import sqlite3
 app = Flask(__name__)
 @app.route('/register', methods=['GET','POST'])
 def register():
     if request.method == 'GET':
         return render_template('register.html')
     elif request.method == 'POST':
+        
         account = request.form['account']
         password = request.form['password']
         phone = request.form['phone']
         address = request.form['address']
+
+        conn = sqlite3.connect('test.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO groups(account,password,address,phone) VALUES \
+                  (?, ?, ?, ?)",(account,password,address,phone))
+        conn.commit()
+        print ("数据插入成功")
+        conn.close()
     
         data = {
             "account": account,
--- HW4/run_app.py
+++ HW4/run_app.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     app.debug = True
     host = os.environ.get('IP', '0.0.0.0')
     port = int(os.environ.get('PORT', 8080))
-    app.run(host=host, port=port)
+    app.run(ssl_context=('server.crt','server.key'), port=port)
